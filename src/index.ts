58fd55b8db34d3de973d63c4402a54f44fd878fd
Remco Hoff <remco@code010.nl>
2021-03-31 17:57:07 +0200

    Fix typo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Timeline from "./components/Timeline/Timeline"
 import TimelineEvents from "./components/EventsTimeline/TimelineEvents"
-import DateDisplay, {displayDate, displayTime, invalidDateText, isValidDate} from "./components/DateDisplay/DateDisplay"
+import DateDisplay, { displayDate, displayTime, invalidDateText, isValidDate } from "./components/DateDisplay/DateDisplay"
 import DayDisplay from "./components/DayDisplay/DayDisplay"
 import CaseIdDisplay from "./components/CaseIdDisplay/CaseIdDisplay"
 
