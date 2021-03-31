4804b4bd6c8e5f11d654e2fff852b40357b5b12e
Remco Hoff <remco@code010.nl>
2021-03-31 17:50:30 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import Timeline from "./components/Timeline/Timeline"
 import TimelineEvents from "./components/EventsTimeline/TimelineEvents"
-import DateDisplay from "./components/DateDisplay/DateDisplay"
+import DateDisplay, {displayDate, displayTime, invalidDateText, isValidDate} from "./components/DateDisplay/DateDisplay"
 import DayDisplay from "./components/DayDisplay/DayDisplay"
 import CaseIdDisplay from "./components/CaseIdDisplay/CaseIdDisplay"
 
 export * from "./components/Timeline/TimelineStyle"
 export {
   DateDisplay,
+  displayDate, 
+  displayTime, 
+  invalidDateText, 
+  isValidDate,
   DayDisplay,
   Timeline,
   TimelineEvents,
