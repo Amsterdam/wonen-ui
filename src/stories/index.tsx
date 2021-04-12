fcaf4a4bcfd8152a7d142905beabef3ac504097f
Remco Hoff <remco@code010.nl>
2021-04-12 14:42:53 +0200

    Update index.tsx

--- src/stories/index.tsx
+++ src/stories/index.tsx
@@ -15,7 +15,7 @@ export type TimelineEventItem = {
 const eventsTimelineData: TimelineEventItem[] = [{ type:"DEBRIEFING", caseEvents:[{ id:2532,"event_values":{ "author":"Remco Hoff","date_added":"2021-03-26T19:19:26.345631Z","violation":"YES","feedback":"ioyhkjh 2" }, date_created: "2021-03-26T20:19:26+0100", emitter_id: 317, case: 3394, type:"DEBRIEFING" },{ id:2533,"event_values":{ "author":"Remco Hoff","date_added":"2021-03-27T19:19:26.345631Z","violation":"YES","feedback":"ioyhkjh 2" }, date_created: "2021-03-26T20:19:26+0100", emitter_id: 317, case: 3394, type:"DEBRIEFING" }] }, { type:"CASE", caseEvents:[{ "id":2264,"event_values":{ "start_date":"2021-03-17","end_date":null,"reason":"Melding","description":"19:31","author":"Remco Hoff" },"date_created":"2021-03-17T19:31:11+0100","type":"CASE","emitter_id":3394,"case":3394 }  ] }, { type:"DECISION", caseEvents:[{ "id":222,"event_values":{ "author":"Remco Hoff","date_added":"2021-04-08T08:56:54.371354Z","persons":["Donald Duck"],"description":"Toelichting","type":"Boete","sanction_amount":456.0 },"date_created":"2021-04-08T10:56:54+0200","type":"DECISION","emitter_id":3394,"case":3394 }] } ]
 
 storiesOf("TimelineEvents", module).add("Tijdlijn", () => (
-  <TimelineEvents items={ eventsTimelineData } />
+  <TimelineEvents items={ eventsTimelineData } spacingHorizontal={6} />
 ))
 
 storiesOf("Timeline", module).add("Tijdlijn", () => (
