3d2ff69703a7dfd6a030fa6abbbabf5696cad6ce
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-07-01 13:53:39 +0200

    Storybook eventstimeline data

---
+++ src/stories/eventsTimelineData.ts
@@ -0,0 +1,280 @@
+import { CaseEvent } from "../components/EventsTimeline/TimelineEvent"
+
+type TimelineEventItem = {
+  type: string
+  caseEvents: CaseEvent[]
+}
+
+const eventsTimelineData: TimelineEventItem[] = [
+	{
+	  type: "SUMMON",
+	  caseEvents:[
+	    {
+	      case: 3898,
+	      date_created: "2021-06-22T12:05:11+0200",
+	      event_values: {
+          author: "Adam",
+          date_added: "2021-06-22T10:05:11.364215Z",
+          description: "Korte toelichting..",
+          number_of_accommodations: 6,
+          persons: ["Donald Duck"],
+          type: "Sluiting"
+	      },
+	      emitter_id: 213,
+	      id: 4884,
+	      type: "SUMMON"
+	    }
+	  ]
+	},
+	{
+	  type: "CASE_CLOSE",
+	  caseEvents:[
+	    {
+	      id: 2221,
+	      event_values: {
+          author: "A. Beecee",
+          date_added: "2021-04-08T08:56:54.371354Z",
+          description: "Toelichting",
+          reason: "Reden..",
+          result: "Resultaat.."
+	      },
+	      date_created: "2021-04-08T10:56:54+0200",
+	      type: "CASE_CLOSE",
+	      emitter_id: 3394,
+	      case: 3394
+	    }
+	  ]
+	},
+	{
+	  type: "DECISION",
+	  caseEvents:[
+	    {
+	      id: 222,
+	      event_values: {
+          author: "A. Beecee",
+          date_added: "2021-04-08T08:56:54.371354Z",
+          persons: ["Donald Duck"],
+          description: "Toelichting",
+          type: "Boete",
+          sanction_amount: 45678,
+          sanction_id: "AZA30-4"
+	      },
+	      date_created: "2021-04-08T10:56:54+0200",
+	      type: "DECISION",
+	      emitter_id: 3394,
+	      case: 3394
+	    }
+	  ]
+	},
+	{
+	  type: "SCHEDULE",
+	  caseEvents: [
+	    {
+	      id: 4040,
+	      event_values: {
+          date_added: "2021-05-20T12:31:45.232760Z",
+          action: "Bezoek",
+          week_segment: "Doordeweeks",
+          day_segment: "Overdag",
+          priority: "Normaal",
+          author: "Adam",
+          description: "Toelichting"
+	      },
+	      date_created: "2021-05-20T14:31:45+0200",
+	      type: "SCHEDULE",
+	      emitter_id: 219,
+	      case: 3785
+	    }
+	  ]
+	},
+	{
+	  type: "CITIZEN_REPORT",
+	  caseEvents: [
+	    {
+	      id: 3956,
+	      event_values: {
+          date_added: "2021-05-18T09:52:03.154320Z",
+          identification: 123456,
+          reporter_name: "Donald Duck",
+          reporter_phone: "1234567890",
+          advertisement_linklist: [
+            "http://www.example.com",
+            "http://www.example.com"
+          ],
+          description_citizenreport: "Korte toelichting..",
+          author: "Adam"
+	      },
+	      date_created: "2021-05-18T11:52:03+0200",
+	      type: "CITIZEN_REPORT",
+	      emitter_id: 1,
+	      case: 3766
+	    }
+	  ]
+	},
+	{
+	  type: "GENERIC_TASK",
+	  caseEvents: [
+	    { id: 437,
+	      event_values: {
+		author: "Local User",
+		date_added: "2021-05-11T12:54:04.349717Z",
+		description: "Dynamisch formulier 1",
+		variables: {
+		  field_1: {
+		    label: "Dit is het label",
+		    value: "Eerste veld",
+		    value_verbose: "Eerste veld (verbose)"
+		  },
+		  field_2: {
+		    label: "Dit is het label voor een boolean",
+		    value: true,
+		    value_verbose: true
+		  },
+		  field_3: {
+		    label: "Label Enum",
+		    value: "waarde_2",
+		    value_verbose: "Waarde 2"
+		  }
+		}
+	      },
+	      date_created: "2021-05-11T14:54:04+0200",
+	      type: "GENERIC_TASK",
+	      emitter_id: 137,
+	      case: 90
+	    }
+	  ]
+	},
+	{
+	  type: "DEBRIEFING",
+	  caseEvents: [
+	    { id: 2535,
+	      event_values: {
+		author: "A. Beecee",
+		date_added: "2021-03-26T19:19:26.345631Z",
+		violation: "YES",
+		feedback: "Hier de opmerkingen.."
+	      },
+	      date_created: "2021-03-26T20:19:26+0100",
+	      emitter_id: 317,
+	      case: 3394,
+	      type: "DEBRIEFING"
+	    },
+	    { id: 2533,
+	      event_values: {
+		author: "A. Beecee",
+		date_added: "2021-03-27T19:19:26.345631Z",
+		violation: "YES",
+		feedback: "Hier de opmerkingen..."
+	      },
+	      date_created: "2021-03-26T20:19:26+0100",
+	      emitter_id: 317,
+	      case: 3394,
+	      type: "DEBRIEFING"
+	    }
+	  ]
+	},
+	{
+	  type: "DEBRIEFING",
+	  caseEvents: [
+	    { id: 2532,
+	      event_values: {
+		author: "A. Beecee",
+		date_added: "2021-03-26T19:19:26.345631Z",
+		violation: "SEND_TO_OTHER_THEME",
+		feedback: "Advertentie gevonden op https://www.example.com, en http://www.example.com/two.",
+		theme: "Kamerverhuur"
+	      },
+	      date_created: "2021-03-26T20:19:26+0100",
+	      emitter_id: 317,
+	      case: 3394,
+	      type: "DEBRIEFING"
+	    },
+	    { id: 2533,
+	      event_values: {
+		author: "A. Beecee",
+		date_added: "2021-03-27T19:19:26.345631Z",
+		violation: "ADDITIONAL_VISIT_WITH_AUTHORIZATION",
+		feedback: "Hier de opmerkingen..."
+	      },
+	      date_created: "2021-03-26T20:19:26+0100",
+	      emitter_id: 317,
+	      case: 3394,
+	      type: "DEBRIEFING"
+	    }
+	  ]
+	},
+	{
+	  type: "VISIT",
+	  caseEvents: [
+	    {
+        id: 227,
+	      event_values: {
+          start_time: "2021-01-01T11:34:00Z",
+          authors: [
+            "Alice",
+            "Bob"
+          ],
+          situation: "access_denied",
+          observations: [],
+          can_next_visit_go_ahead: true,
+          can_next_visit_go_ahead_description: null,
+          suggest_next_visit: null,
+          suggest_next_visit_description: null
+	      },
+	      date_created: "2021-04-08T11:25:07+0200",
+	      type: "VISIT",
+	      emitter_id: 81,
+	      case: 3394
+	    },
+      { id: 228,
+	      event_values: {
+          start_time: "2021-01-02T11:34:00Z",
+          authors: [
+            "Adam",
+            "Eva"
+          ],
+          situation: "access_granted",
+          observations: [],
+          can_next_visit_go_ahead: true,
+          can_next_visit_go_ahead_description: null,
+          suggest_next_visit: null,
+          suggest_next_visit_description: null
+	      },
+	      date_created: "2021-04-08T11:25:07+0200",
+	      type: "VISIT",
+	      emitter_id: 81,
+	      case: 3394
+	    }
+	  ]
+	},
+	{
+	  type: "CASE",
+	  caseEvents:[
+	    {
+	      id: 2264,
+	      event_values: {
+          start_date: "2021-03-17",
+          end_date: null,
+          reason: "Project",
+          project: "Levant",
+          description: "19:31",
+          author: "A. Beecee",
+          identification: 123456,
+          reporter_name: "Donald Duck",
+          reporter_phone: "1234567890",
+          advertisement_linklist: [
+            "www.example.com",
+            "http://www.example.com",
+            "https://www.example.com"
+          ]
+	      },
+	      date_created: "2021-03-17T19:31:11+0100",
+	      type: "CASE",
+	      emitter_id: 3394,
+	      case: 3394
+	    }
+	  ]
+	}
+]
+
+export default eventsTimelineData
