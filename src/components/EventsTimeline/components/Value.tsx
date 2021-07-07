834db92143f6f7f46d300664d13c9651b3e9569a
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-07-07 22:48:46 +0200

    Indention

--- src/components/EventsTimeline/components/Value.tsx
+++ src/components/EventsTimeline/components/Value.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 
 type Props = {
-	value: React.ReactNode
-	displayItalic?: boolean
+  value: React.ReactNode
+  displayItalic?: boolean
 }
 
 const Value: React.FC<Props> = ({ value, displayItalic = false }) => displayItalic ? <i>{ value }</i> : <>{ value }</>
