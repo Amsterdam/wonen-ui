5afb510aeafa5068a2dd3633164c21665ad4e507
Remco Hoff <remco@code010.nl>
2021-07-13 16:44:56 +0200

    Cleanup code

--- src/stories/eventsTimelineData.ts
+++ src/stories/eventsTimelineData.ts
@@ -10,14 +10,7 @@ const eventsTimelineData: CaseEvent[] = [
       project: "Levant",
       description: "19:31",
       author: "A. Beecee",
-      identification: 123456,
-      reporter_name: "Donald Duck",
-      reporter_phone: "1234567890",
-      advertisement_linklist: [
-        "www.example.com",
-        "http://www.example.com",
-        "https://www.example.com"
-      ]
+      identification: 123456
     },
     date_created: "2021-03-17T19:31:11+0100",
     type: "CASE",
