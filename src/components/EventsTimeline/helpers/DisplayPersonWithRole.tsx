73518a93e0e8d1ce12bd76f3a50aa92033202ce2
Remco Hoff <remco@code010.nl>
2021-07-13 15:41:30 +0200

    Update model of Person

--- src/components/EventsTimeline/helpers/DisplayPersonWithRole.tsx
+++ src/components/EventsTimeline/helpers/DisplayPersonWithRole.tsx
@@ -1,12 +1,15 @@
 import { PersonRole, personRoleMap } from "../../../helpers/dictionaries"
 
 export type Person = {
-  person_full_name: string
-  person_role: PersonRole
+  first_name?: string
+  preposition?: string
+  last_name: string
+  person_role?: PersonRole
 }
 
-const DisplayPersonWithRole = (person: Person) => 
-  // TODO use PersonNameDisplay (with person_full_name) and PersonRoleDisplay
-  `${ person.person_full_name }, ${ typeof person.person_role === "string" ? personRoleMap[person.person_role].toLowerCase() : "-" }`
-  
+const DisplayPersonWithRole = (person: Person) => {
+  const { first_name, last_name, preposition, person_role } = person
+  // TODO use PersonNameDisplay and PersonRoleDisplay
+  return  `${ first_name ?  `${ first_name } ` : "" } ${ preposition ? `${ preposition } ` : "" }${ last_name }, ${ typeof person_role === "string" ? personRoleMap[person_role].toLowerCase() : "-" }`
+}
 export default DisplayPersonWithRole
