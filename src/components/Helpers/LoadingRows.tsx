bd8720fb9933161047e24b0ae94934c32cc18e23
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-07-19 16:38:16 +0200

    Move DefinitionList

--- src/components/Helpers/LoadingRows.tsx
+++ src/components/Helpers/LoadingRows.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import SmallSkeleton from "./SmallSkeleton"
-import Definition from "../DefinitionList/components/Definition"
+import Definition from "../Data/DefinitionList/components/Definition"
 
 type Props = {
   numRows: number
