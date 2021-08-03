23dcc46baffa402734f3dd15af5bdb00ac169bc2
Remco Hoff <remco@code010.nl>
2021-08-03 11:57:27 +0200

    Create PersonNameDisplay.test.tsx

---
+++ src/components/PersonNameDisplay/tests/PersonNameDisplay.test.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { shallow } from "enzyme"
+import PersonNameDisplay from "../PersonNameDisplay"
+
+describe("FinancialDisplay", () => {
+  let component = shallow(<PersonNameDisplay sex="V" firstName="A." name="Beecee"/>)
+
+  it("should render component with first- and lastname", () => {
+    expect(component.text()).toContain("A.")
+    expect(component.text()).toContain("Beecee")
+  })
+  it("should render component with first- and lastname and prefix", () => {
+    component.setProps({ namePrefix: "van" })
+    expect(component.text()).toContain("van")
+  })
+})
