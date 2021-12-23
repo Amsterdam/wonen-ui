2cc3a5a92e94db45c8f9a367c3c71a75ce211393
Remco Hoff <remco@code010.nl>
2021-12-23 11:19:09 +0100

    Update dictionaries.ts

--- src/helpers/dictionaries.ts
+++ src/helpers/dictionaries.ts
@@ -1,7 +1,8 @@
 export const personRoleMap = {
   "PERSON_ROLE_OWNER": "Eigenaar",
   "PERSON_ROLE_RESIDENT": "Bewoner",
-  "PERSON_ROLE_MIDDLEMAN": "Tussenpersoon"
+  "PERSON_ROLE_MIDDLEMAN": "Tussenpersoon",
+  "PERSON_ROLE_PLATFORM": "Platform"
 }
 
 export type PersonRole = keyof typeof personRoleMap
