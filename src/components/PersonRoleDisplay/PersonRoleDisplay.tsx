4787a5a7b88b0d7ff53e7f69e848738478a7fa79
remyvdwereld <86827854+remyvdwereld@users.noreply.github.com>
2022-02-10 12:08:32 +0100

    WZ-1815

--- src/components/PersonRoleDisplay/PersonRoleDisplay.tsx
+++ src/components/PersonRoleDisplay/PersonRoleDisplay.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { PersonRole, personRoleMap } from "../../helpers/dictionaries"
 
-
 type Props = {
   personRole?: PersonRole | undefined | null
 }
 
-const PersonRoleDisplay: React.FC<Props> = ({ personRole }) =>
+const PersonRoleDisplay: React.FC<Props> = ({ personRole }) => (
   <>{ personRole ? `${ (personRoleMap[personRole]).toLowerCase() }` : "" }</>
+)
 
 export default PersonRoleDisplay
