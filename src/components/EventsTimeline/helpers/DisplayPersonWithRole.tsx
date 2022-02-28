75975c97eae2a43209e47c203245f37cbfe8d475
Daan Beverdam <daanbeverdam@gmail.com>
2022-02-28 20:52:04 +0100

    fixed linting

--- src/components/EventsTimeline/helpers/DisplayPersonWithRole.tsx
+++ src/components/EventsTimeline/helpers/DisplayPersonWithRole.tsx
@@ -1,6 +1,6 @@
 import PersonRoleDisplay from "../../../components/PersonRoleDisplay/PersonRoleDisplay"
 import PersonNameDisplay from "../../../components/PersonNameDisplay/PersonNameDisplay"
-import PersonEntityDisplay from "../../../components/PersonEntityDisplay/PersonEntityDisplay
+import PersonEntityDisplay from "../../../components/PersonEntityDisplay/PersonEntityDisplay"
 import { PersonRole } from "../../../helpers/dictionaries"
 
 export type Person = {
@@ -14,10 +14,12 @@ export type Person = {
 
 const DisplayPersonWithRole = (person: Person) => {
   const { first_name, last_name, preposition, person_role, entity_name, function: personFunction } = person
-  return (<>
-    <PersonNameDisplay firstName={first_name} namePrefix={preposition} name={last_name} />,
-    <PersonEntityDisplay personFunction={personFunction} entityName={entity_name} />,
-    <PersonRoleDisplay personRole={person_role} />
-  </>)
+  return (
+    <>
+      <PersonNameDisplay firstName={first_name} namePrefix={preposition} name={last_name || ""} />,
+      <PersonEntityDisplay personFunction={personFunction} entityName={entity_name} />,
+      <PersonRoleDisplay personRole={person_role} />
+    </>
+  )
 }
 export default DisplayPersonWithRole
