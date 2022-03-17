6ece313e29b0c9fdfc420cba281f9c85e3083966
remyvdwereld <86827854+remyvdwereld@users.noreply.github.com>
2022-03-17 11:20:49 +0100

    Removed comments

--- src/components/CaseIdDisplay/CaseIdDisplay.tsx
+++ src/components/CaseIdDisplay/CaseIdDisplay.tsx
@@ -4,9 +4,6 @@ type Props = {
   id?: string | number
 }
 
-  // const idLength = 6
-  // id?.toString().padStart(idLength, "0") to create an ID of 6 figures.
-
 const CaseIdDisplay: React.FC<Props> = ({ id }) => <>{ id ?? "-" }</>
 
 export default CaseIdDisplay
