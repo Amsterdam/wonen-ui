220b7c36e8f8f23b9a62df83ac5f1aff679c67d1
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2024-09-09 12:45:17 +0200

    Added role landlord verhuurder

--- src/helpers/dictionaries.ts
+++ src/helpers/dictionaries.ts
@@ -3,7 +3,8 @@ export const personRoleMap = {
   "PERSON_ROLE_RESIDENT": "Bewoner",
   "PERSON_ROLE_MIDDLEMAN": "Tussenpersoon",
   "PERSON_ROLE_PLATFORM": "Platform",
-  "PERSON_ROLE_HEIR": "Erfgenaam"
+  "PERSON_ROLE_HEIR": "Erfgenaam",
+  "PERSON_ROLE_LANDLORD": "Verhuurder"
 }
 
 export type PersonRole = keyof typeof personRoleMap
