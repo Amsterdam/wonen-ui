35c9c8712741c9b69f3763d9b1f646662db400e7
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2025-07-23 17:22:19 +0200

    Add additional schedule and visit events

--- src/components/EventsTimeline/helpers/dictionaries.ts
+++ src/components/EventsTimeline/helpers/dictionaries.ts
@@ -3,9 +3,11 @@ export const caseTypesMap = {
   "CASE": "Aanleiding",
   "CASE_CLOSE": "Zaak afgerond",
   "SCHEDULE": "Bezoek ingepland",
+  "SCHEDULE_ADDITIONAL": "Aanvullend bezoek ingepland",
   "DEBRIEFING": "Debrief",
   "SUMMON": "Aanschrijving",
   "VISIT": "Bezoek",
+  "VISIT_ADDITIONAL": "Aanvullend bezoek",
   "DECISION": "Besluit",
   "CITIZEN_REPORT": "SIG melding verwerken"
 }
